01a21b0b032f24225d75518c95270f8f487b260c
Сергей Вертинский <vertinski96@gmail.com>
2020-05-28 18:01:15 +0300

    Start

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from testapp.views import test
+from test import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
